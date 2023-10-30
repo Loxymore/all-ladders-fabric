9b998996efb617bc5685dcafd51d64bf1a5ffaa8
Loxymore <loxtv91@gmail.com>
2023-10-30 23:48:23 +0100

    Initial commit

---
+++ src/main/java/net/loxymore/all_ladders/AllLadders.java
@@ -0,0 +1,16 @@
+package net.loxymore.all_ladders;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AllLadders implements ModInitializer {
+	public static final String MOD_ID = "all-ladders";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/loxymore/all_ladders/AllLaddersClient.java
@@ -0,0 +1,10 @@
+package net.loxymore.all_ladders;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AllLaddersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
